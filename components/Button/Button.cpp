02e8fb92c300bc9ee17da0376a73d2e37e3b249e
FilemonG <159620607+FilemonG@users.noreply.github.com>
2025-02-12 14:00:37 +0100

    gpio

--- components/Button/Button.cpp
+++ components/Button/Button.cpp
@@ -4,7 +4,10 @@
 void Button::init()
 {
     gpio_config_t buttonConf;
-
+        buttonConf.mode = GPIO_MODE_INPUT;
+        buttonConf.pull_up_en = GPIO_PULLUP_ENABLE;
+        buttonConf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+        buttonConf.intr_type = GPIO_INTR_NEGEDGE;
 }
 void Button::button_uppdate(){
     
--- components/Button/include/Button.h
+++ components/Button/include/Button.h
@@ -14,7 +14,6 @@ class Button{
 
     Button(int pin, gpio_pullup_t pull_up_en, gpio_pulldown_t pull_down_en, gpio_int_type_t intr_type) :
         pinNum(pin), pull_up_en(pull_up_en), pull_down_en(pull_down_en), intr_type(intr_type){ mode = GPIO_MODE_INPUT;}
-
     void init();
     void button_uppdate(void);
     bool isPressed(void);
