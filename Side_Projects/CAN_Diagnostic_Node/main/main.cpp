58280b573d63e84bb3f2f5164a1fd5c4492a240c
FilemonG <filimongonetse@gmail.com>
2025-12-16 15:50:58 +0100

    Big changges

---
+++ Side_Projects/CAN_Diagnostic_Node/main/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "driver/twai.h"
+
+#define ANSI_GREEN "\033[0;32m"
+#define ANSI_Yellow "\033[0;33m"
+#define ANSI_Red "\033[0;31m"
+
+using namespace std;
+
+
+extern "C"
+{
+void app_main(void)
+{
+    twai_handle_t my_twai_bus
+}
+};
+
